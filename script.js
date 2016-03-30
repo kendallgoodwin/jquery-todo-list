f1aff6bc56349b265b0cf3003b0dc40f7bdda4de
kendallgoodwin <kendall@kendallgoodwin.com>
2016-03-29 18:57:02 -0700

    basic styling, no buttons yet, js works

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function() {
+
+});
+
+$('#myForm').submit(function(event) {
+	event.preventDefault();
+
+	$(this).children('input:text').each(function(index, elem) {
+		var value = $(elem).val();
+		var newThing = $('<li>' + value + '</li>')
+		$('ul').append(newThing);
+		$(elem).val('');
+	});
+
+	console.log(event);
+});
+
+$('ul').on('click', 'li', function(event) {
+	$(this).remove();
+});
